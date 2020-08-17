f74918e801b6a945dbe8aca75962c2e631652778
James <contact@james-wright.co.uk>
2020-08-17 06:56:19 +0100

    more work

--- api/functions/booking/booking.js
+++ api/functions/booking/booking.js
@@ -1,9 +1,15 @@
 const express = require("express");
 let router = express.Router();
 const fb = require('../admin');
+const rateLimit = require("express-rate-limit");
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 5 minutes
+  max: 100 // limit each IP to 2 requests per windowMs
+});
 
-router.use( async(req, res, next) => {
+router.use(limiter);
+const checkAuth = async (req, res, next) => {
   const token = req.get('X-API');
   if(!token) {
     res.status(401).send();
@@ -17,7 +23,9 @@ router.use( async(req, res, next) => {
     console.log(e);
     res.status(403).send(e);
   }
-})
+}
+
+router.use(checkAuth);
 
 router
   .route("/")
